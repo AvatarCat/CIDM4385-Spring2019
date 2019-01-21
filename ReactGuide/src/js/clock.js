28e9f1ed2b1206e462a78699bd8af096b9493e0d
jeff <ahuimanu@gmail.com>
2019-01-20 23:20:49 -0600

    updated for State and Lifecycle from the React tutorial

--- ReactGuide/src/js/clock.js
+++ ReactGuide/src/js/clock.js
@@ -1,18 +1,41 @@
 class Clock extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(), 
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+
     render() {
         return (
             <div>
-                <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
         );
     }
 }
 
-function tick() {
-    ReactDOM.render(
-        <Clock date={new Date()} />, 
-        document.getElementById('timeslot2')
-    );
-}
+ReactDOM.render(
+    <Clock />, 
+    document.getElementById('timeslot2')
+);
 
-setInterval(tick, 1000);
+function tick() {
+
+}
